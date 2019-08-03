d085dabf50cc7926bb2f43a5dd47504baaa7d9af
didierdelhaisse <did35@yahoo.com>
2019-08-03 23:27:12 +0200

    First commit

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
